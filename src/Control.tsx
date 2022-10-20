29141691d506ff751ab68e8aaac2e96b5879b69e
Filip <filfer05@gmail.com>
2022-10-20 15:44:52 +0200

    translate rotate

--- src/Control.tsx
+++ src/Control.tsx
@@ -1,41 +1,80 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 
-interface Control {
+interface ControlProps {
     element: JSX.Element,
     opacity?: number,
     duration?: number,
     delay?: number,
-    ease?: "ease-in" | "ease-out" | "ease-in-out"
+    ease?: "ease-in" | "ease-out" | "ease-in-out",
+    mount?: number,
+    x?: number | string,
+    y?: number | string,
+    rotate?: number
 }
 
-type Styles = Omit<Control, 'element'>
+type S = Omit<ControlProps, 'element' | 'mount' | 'x' | 'y' | 'rotate'>
 
-export default function Control(props:Control):JSX.Element {
-    const { element, opacity, duration, ease, delay } = props;
+interface Styles extends S {
+    transform?: string
+}
 
-    const wrapper = useRef<HTMLDivElement | null>(null)
+export default function Control(props:ControlProps):JSX.Element {
+    const { element, mount, opacity, duration = 400, ease = 'cubic-bezier(0, 0, 1.0, 1.0)', delay = 0, x, y, rotate = 0 } = props;
+
+    const firstRender = useRef<boolean>(true)
+    const timer = useRef<number | undefined>(undefined)
+
+    // default values for styles
 
     const [styles, setStyles] = useState<Styles>({
-        opacity: opacity
+        opacity: 0
     })
 
-    useLayoutEffect(() => {
-        if(opacity) setStyles(prev => {
+    useEffect(() => {
+
+        // testing purposes; setting default values for styles
+
+        if(!firstRender.current) setStyles(prev => {
             return {
                 ...prev,
-                opacity: 0
+                opacity: 0,
+                transform: 'none'
             }
         })
-    }, [])
+
+        firstRender.current = false
+
+        // different cases for x and y since you can provide a number or a string with sth like % value
+
+        let xValue: number | string = typeof x === 'number' ? x + 'px' : typeof x === 'string' ? x : ''
+        let yValue: number | string = typeof y === 'number' ? y + 'px' : typeof y === 'string' ? y : ''
+
+        // full transform syntax for all cases
+
+        let translateValue: string = x && y ? `translate(${xValue}, ${yValue})` : y ? `translateX(${yValue})` : x ? `translateX(${xValue})` : ''
+
+        // rotate value
+
+        let rotateValue: string = `rotate(${rotate}deg)`
+
+        // changing values of different style properties
+
+        timer.current = setTimeout(() => {
+            setStyles(prev => {
+                return {
+                    ...prev,
+                    opacity: opacity,
+                    transform: `${translateValue} ${rotateValue}`
+                }
+            })
+        }, 400)
+
+        return () => clearTimeout(timer.current)
+    }, [mount])
 
     useEffect(() => {
-        if(opacity) setStyles(prev => {
-            return {
-                ...prev,
-                opacity: opacity
-            }
-        })
-    }, [])
+        console.log(styles)
+    }, [styles])
 
-    return <div ref={wrapper} style={{...styles, transition: `all ${duration}ms ${ease} ${delay}ms`}}>{element}</div>
+    return <div style={{...styles, transition: `all ${duration}ms ${ease} ${delay}ms`, maxWidth: 'max-content'}}>{element}</div>
 }
--- test/src/App.tsx
+++ test/src/App.tsx
@@ -1,10 +1,13 @@
 import Control from "control"
+import { useState } from "react"
 
 export default function App() {
+  const [mount, setMount] = useState(1)
   return (
-    <>
+    <div className="app">
       <h1>Testing ControlJS Library</h1>
-      <Control element={<p>Hello World</p>} opacity={1} duration={600} delay={400} ease='ease-in' />
-    </>
+      <button onClick={() => setMount(prev => prev + 1)}>ReMount</button>
+      <Control element={<p>Hello World</p>} mount={mount} x={20} y={20} rotate={-50} ease='ease-out' opacity={1} />
+    </div>
   )
 }
